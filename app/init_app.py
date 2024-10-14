57586a028f4d130689fff5748132f02ad8e3b270
quickSilverShanks <shashank708.2606@gmail.com>
2024-10-15 03:50:12 +0530

    updated streamlit initialization
    - added mock script start.sh
    - updated streamlit inilialization functionality

--- app/init_app.py
+++ app/init_app.py
@@ -1,9 +1,11 @@
 import streamlit as st
+import time
 
 def initialize_list():
     if 'video_options' not in st.session_state:
         # Initialize the list if it's not already present in session_state
         st.session_state['video_options'] = ['Video 1', 'Video 2', 'Video 3']
+        time.sleep(10)
 
 # Call the function to initialize
 initialize_list()
--- app/vidsage_ui.py
+++ app/vidsage_ui.py
@@ -1,7 +1,6 @@
 import streamlit as st
-import init_temp
+import init_app
 
-init_temp.initialize_list()
 
 home = st.Page("home.py", title="Home", icon="🏠")
 add_vid = st.Page("add_video.py", title="Add Video", icon="📜")
@@ -11,3 +10,29 @@ rag_bot = st.Page("ai_assistant.py", title="AI Assistant", icon="🤖")
 pg = st.navigation([home, add_vid, rag_bot])
 st.set_page_config(page_title="VidSage", page_icon="💬")
 pg.run()
+
+
+# Initialize session state for tracking initialization
+if 'initialized' not in st.session_state:
+    st.session_state.initialized = False
+
+
+# Initialize sidebar message container
+sidebar_message = st.sidebar.empty()
+
+# Display a spinner while the initialization is running
+if not st.session_state.initialized:
+    with sidebar_message.container():
+        with st.spinner("Initializing Application..."):
+            init_app.initialize_list()
+            
+        # Change the spinner to a "Ready..." message
+        sidebar_message.markdown(
+            "<div style='background-color: #77aa44; padding: 7px; border-radius: 5px;'>"
+            "<h7 style='margin: 0;'>Ready...</h7>"
+            "</div>",
+            unsafe_allow_html=True
+        )
+
+    # Set the initialized flag to True
+    st.session_state.initialized = True
