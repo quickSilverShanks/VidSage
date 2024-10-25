ad138e7e5e2c21f65e3ac091407679bfd9ab792e
quickSilverShanks <shashank708.2606@gmail.com>
2024-10-25 18:00:12 +0530

    added pgsql database support
    - added postgreSQL db support
    - modified docker compose file
    - added new test data
    - fixed bug in transcript fetch
    - modified utils

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -3,5 +3,8 @@
 # Run the initialization script ; deprecated initialization
 # python init-app.py
 
+# Start Flask in the background on port 5000
+# gunicorn --bind 0.0.0.0:5000 add_video_active:app_getvid &
+
 # Run Streamlit and make it respond properly to shutdown signals
 exec streamlit run vidsage_ui.py --server.port=8501
--- app/vidsage_ui.py
+++ app/vidsage_ui.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import utils.db as db
-import utils.init_app_local as init_app
-# import utils.init_app as init_app
+# import utils.init_app_local as init_app
+import utils.init_app as init_app
 
 
 home = st.Page("home.py", title="Home", icon="🏠")
